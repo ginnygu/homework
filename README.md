616691f07a263c463edc30081e0b5bac47bc0fb1
Ginny Gu Tang <ginnygu@gmail.com>
2019-05-16 16:51:26 -0400

    finished with readme and instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # homework
+
+1) Check if you have node.js globally in your Mac. You can tell in your terminal by typing node -v. That should tell you which version you are have. If not do brew install node. 
+
+2) Fork this repo and start!
+
+3) Make functions to complete the challenges in main.js. You will have to comment them out once you're done with each challenge. For each one, you can test it with console.log and by running `node main.js` in your terminal. Remember you need to be in the folder to run that! 
+
+4) Commit after every challenge!
+
+5) Link me to your repo when you're done! Good Luck and no slacking!!!
+
+P.S: If you need help/clarification or any questions, message me through slack!
---
+++ main.js
@@ -0,0 +1,360 @@
+// example: 
+
+// function myFunction(x, y = 10) {
+//     return x + y;
+// }
+// myFunction(5); // will return 15
+
+
+// challenge #1
+// Here's an array:
+// numbers = [4, 9, 16, 23, 29];
+// use the .find() method to find a value less than 18
+// //will return 4
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#2
+// using the same array from above, use .findIndex() to return the index of a value less than 18
+// //will return 3
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#3
+// Here's an array:
+// words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// use the  .filter() method to return any words that have more than 6 letters.
+// //expected output: Array ["exuberant", "destruction", "present"]
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#4
+// Here's an array:
+// ages = [32, 33, 16, 40];
+// use the .filter() method to return any age greater or equal to 18. *make a function called checkAdult.
+// //expected output: Adults: [32, 33, 40]
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#5
+// Here's an array of  objects:
+// arr = [
+//     { id: 15 },
+//     { id: -1 },
+//     { id: 0 },
+//     { id: 3 },
+//     { id: 12.2 },
+//     { },
+//     { id: null },
+//     { id: NaN },
+//     { id: 'undefined' }
+//   ];
+// use the  .filter() method to only return id's with  #
+// ### expected output: [{ id: 15 }, { id: -1 }, { id: 3 }, { id: 12.2 }]  
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#6
+// Here's an array of objects:
+// heroes = [
+// 	{name: "Batman", franchise: "DC"},
+// 	{name: "Ironman", franchise: "Marvel"},
+// 	{name: "Thor", franchise: "Marvel"},
+// 	{name: "Superman", franchise: "DC"}
+// ];
+// use the .filter() method return only marvelHeroes.
+// expected output:  
+//    marvelHeroes:  [
+//     { name: 'Ironman', franchise: 'Marvel' },
+//     { name: 'Thor', franchise: 'Marvel' }
+//     ]
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#7
+// Here's an array
+// numbers = [5, 15, 55, "515"]
+// Write an extension for array of integers that returns the number of times a specific digit appears in any of its numbers.
+// expected output: 6
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// challenge#8
+// Write a function that accepts a string as its input, and returns the same string just with duplicate letters removed.
+// solution("Mississippi") === "Misp"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//challenge#9
+//Here's an array
+//sweetArray = [2, 3, 4, 5, 35];
+//Use the .map() method to multiply each number by 2.
+//expected output: [4, 6, 8, 10, 70]
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//challenge#10
+//Here's an array of objects
+//officers = [
+//     { id: 20, name: 'Captain Piett' },
+//     { id: 24, name: 'General Veers' },
+//     { id: 56, name: 'Admiral Ozzel' },
+//     { id: 88, name: 'Commander Jerjerrod' }
+// ];
+//Use the .map() method to return only the id 
+//expected output: [20, 24, 56, 88]
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//challenge#11
+//Write a solution function that returns a string with each of its words reversed but in the original order, use .map().
+//expected output; solution("The quick brown fox") == "ehT kciuq nworb xof";
+
+
+//write your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//challenge#12
+//Here's an array
+// officers = [
+//     {id: 20, name:'captain Pet'},
+//     {id: 21, name:'captain Mr'},
+//     {id: 22, name:'captain Pete'},
+//     {id: 23, name:'captain Ger'},
+//     {id: 24, name:'captain Mac'},
+//     {id: 25, name:'captain Cheese'}
+// ]
+//Use .forEach() method and push only the id's to a new array
+//expected output: result[20, 21, 22, 23, 24, 25]
+
+
+//write your  code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
